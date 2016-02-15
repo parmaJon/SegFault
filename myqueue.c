bc2a1bc7cf37922f164dea55aaab600e1d7212cc
Lucas R Stuyvesant <lstuyves@nmt.edu>
2016-02-15 10:34:19 -0700

    function outlines

--- myqueue.c
+++ myqueue.c
@@ -15,3 +15,42 @@
 ******************************************************/
 
 #include "myqueue.h"
+
+/**
+ * Inserts a process into the end of the queue
+ * @param process the process being inserted
+ * @return true if successful, false otherwise
+ */
+bool enqueue(Process process) {
+    return false;
+}
+
+/**
+ * Removes a process from the front of the queue
+ * @return the process removed, or NULL in error
+ */
+Process dequeue() {
+    return NULL;
+}
+
+/**
+ * Removes the process(es) from the queue with the
+ * given pid
+ * @param pid the pid of the process(es) to be deleted
+ */
+void delete(int pid) {
+    return;
+}
+
+/**
+ * Determines whether or not the queue is empty or not
+ * @return true if the queue is empty, false otherwise
+ */
+bool isEmpty() {
+    return false;
+}
+
+
+bool isFull();
+void clear();
+void listQueue();
