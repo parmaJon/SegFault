b915fbe6e1ad7360b348f42996fd886a0e47c5ee
Garrett Ransom <garrettwransom@gmail.com>
2016-03-02 19:08:17 -0700

    Added prod/con functionality

--- segfault_p3/main.c
+++ segfault_p3/main.c
@@ -26,6 +26,7 @@
 #include <pthread.h>
 
 int rand_max;
+int args[3];
 static sem_t empty;
 static sem_t full;
 static sem_t mutex;
@@ -49,9 +50,8 @@ int isNum(char *str) {
 
 int main(int argc, char *argv[]) {
 
-    int args[3];
     int i;
-
+    srand(time(NULL)); //set random seed
 
     /* Get command line arguments */
     if(argc != 4) {
@@ -66,8 +66,13 @@ int main(int argc, char *argv[]) {
         }
         args[i-1] = atoi(argv[i]);
     }
+
+    if( args[2] > 2  ||  args[2] < 0 ) {
+        printf("ERROR: Argument 3 must be either 0-FIFO, 1-RANDOM, or 2-TARGETED\n");
+        return -1;
+    }
     
-    rand_max = args[0] - 1;
+    rand_max = args[1] - 1;
 
     /* Initialize queue */
     myqueue.size = 0;
@@ -79,15 +84,27 @@ int main(int argc, char *argv[]) {
             return -1;
         }
     }
+
+    /* Initialize semaphores */
+    if (sem_init(&empty, 0, MAX_PROCESSES) == -1)
+        perror("sem_init failed for empty\n");
+    if (sem_init(&full, 0, 0) == -1)
+        perror("sem_init failed for full\n");
+    if (sem_init(&mutex, 0, 1) == -1)
+        perror("sem_init failed for mutex\n");
+
     
     /* Create Producer thread(s) */
     
     /* Create Consumer thread(s) */
     
     /* Sleep for 300 seconds */
-    //sleep(3);
+    sleep(3); //temporarily lowered
     
+    /* Terminate all threads */
+
     /* Exit */
+    clear();
     return 0;
 }
 
@@ -97,6 +114,30 @@ int main(int argc, char *argv[]) {
  * @return - Status, 0 if successful.
  */
 void * producer(void * arg) {
+    struct timespec* time = malloc(sizeof(struct timespec));
+    time->tv_nsec = rand() % 1001;
+    time->tv_sec = 0;
+
+    /* wait for the random amount of time */
+    nanosleep(time,time);
+
+    int val = rand()%rand_max;
+    
+    sem_wait(&empty); //check if room in queue
+    sem_wait(&mutex); //begin critical section
+
+    if( enqueue(val) != -1 ) {
+        printf("item (%d) added by Producer %d: queue = ", val, pthread_self());
+        listQueue();
+    }
+    else {
+        printf("Error: Producer %d could not add its value!\n", pthread_self());
+    }
+
+    sem_post(&mutex); //end critical section
+    sem_post(&full);  //increment the count of queue elements
+
+    free(time);
     return 0; 
 }
 
@@ -107,6 +148,36 @@ void * producer(void * arg) {
  * @return - Status, 0 if successful.
  */
 void * consumer(void * arg) {
+    //thread_data_t *data = (thread_data_t *)arg;
+
+    struct timespec* time = malloc(sizeof(struct timespec));
+    time->tv_nsec = rand() % 1001;
+    time->tv_sec = 0;
+
+    /* wait for the random amount of time */
+    nanosleep(time,time);
+
+    wait(&full); //ensure the queue has an element
+    wait(&mutex); //enter critical section
+
+    switch(args[2]) {
+    case 0:
+        printf("item (%d) taken by Consumer %d: queue = ", dequeue(), pthread_self());
+        break;
+    case 1:
+        printf("item (%d) taken by Consumer %d: queue = ", randomTarget(), pthread_self());
+        break;
+    case 2:
+        printf("item (%d) taken by Consumer %d: queue = ", target(pthread_self()), pthread_self());
+        break;
+    default:
+        printf("Consumer %d says - \"Wait...what am I supposed to do?\"", pthread_self());
+    }
+
+    sem_post(&mutex); //end critical section
+    sem_post(&empty); //indicate that queue space is available
+
+    free(time);
     return 0; 
 }
 
